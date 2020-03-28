d329ded6bde24b67b4489db56057ac85a73a194b
stepenshen <stepenshen@foxmail.com>
2020-03-28 21:20:02 +0800

    后台的前端初始化

---
+++ frontend/src/api/profile.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+
+/**
+ * 获取个人信息
+ * @param username
+ * @returns {AxiosPromise}
+ */
+export function info(username) {
+  return request({
+    url: '/profile/info/' + username,
+    method: 'get'
+  })
+}
+
+/**
+ * 更新个人信息
+ * @param username
+ * @returns {AxiosPromise}
+ */
+export function update(data) {
+  return request({
+    url: '/profile/update/',
+    method: 'post',
+    data
+  })
+}
+
+
+
+/**
+ * 更新密码
+ * @param data
+ */
+export function modifyPassword(data) {
+  return request({
+    url: '/profile/modify/password',
+    method: 'post',
+    data
+  })
+}
+
+
+
+/**
+ * 更新头像
+ * @param data
+ */
+export function modifyIcon(data) {
+  return request({
+    url: '/profile/modify/icon',
+    method: 'post',
+    data
+  })
+}
