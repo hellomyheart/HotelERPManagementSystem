e2484d1421e9488a2e68755aee2c8b09bb89edbf
stepenshen <stepenshen@foxmail.com>
2020-03-31 14:15:31 +0800

    文件上传oss

---
+++ cloud/cloud-upload-service/src/main/java/com/demo/hotel/cloud/configure/UploadResourceServerConfiguration.java
@@ -0,0 +1,38 @@
+package com.demo.hotel.cloud.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * @program: HotelERPManagementSystem
+ * @description: 文件上传资源服务器
+ * @author: syj
+ * @create: 2020-03-31 12:41
+ **/
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class UploadResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .exceptionHandling()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").hasAuthority("USER");
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        // 配置资源 ID
+        resources.resourceId("backend-resources");
+    }
+}
